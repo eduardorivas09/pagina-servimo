294f7311224923c670c09b6a9a336e7d587bd34f
Eduardo Rivas <rivaseduardo655@gmail.com>
2020-11-22 14:58:14 -0600

    Se agrego la pagina con react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home/Home';
+import Servicio from "./components/servicio/Servicio";
 import React from "react";
 
 function App() {
   return (
-      <Home/>
+      // <Home/>
+
+      <Servicio/>
+
   );
 }
 
