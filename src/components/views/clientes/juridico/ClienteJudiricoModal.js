9a088c7ac980fd96a877a06dbff15a71f9da4c2b
guismarck <guismarck01@gmail.com>
2021-01-20 22:42:32 -0600

    cambios en cliente

---
+++ src/components/views/clientes/juridico/ClienteJudiricoModal.js
@@ -0,0 +1,71 @@
+import React, { Fragment } from 'react';
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
+import { SelectButton } from "primereact/selectbutton";
+import { Dropdown } from "primereact/dropdown";
+import { Button } from "primereact/button";
+import { InputTextarea } from "primereact/inputtextarea";
+
+export default class ClienteJuridicoModal extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            visible: true
+        }
+        this.onHide = this.onHide.bind(this);
+        this.onYesClick = this.onYesClick.bind(this);
+    }
+
+    onHide = () => {
+        this.setState({
+            visible: true
+        });
+    }
+
+    onYesClick = () => {
+        alert('Sin funcionalidad . Trabajando en ello')
+    }
+
+    renderFooter = () => {
+        return (
+            <div style={{ marginTop: '1em' }}>
+                <Button label="No" icon="pi pi-times" onClick={this.onHide} className="p-button-text" />
+                <Button label="Yes" icon="pi pi-check" onClick={this.onYesClick} autoFocus />
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <Dialog className="p-dialog" role="alert"
+                header={"Persona Juridico"}
+                position="top-bottom"
+                maximizable={true}
+                visible={this.props.visible}
+                style={{ width: '50vw' }}
+                footer={this.renderFooter()}
+                onHide={() => this.props.onHide()} >
+
+                <div className="container m-0">
+                    <div className="row">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-4">
+                            <spa className="p-float-label" style={{ marginTop: '1.3em' }}>
+                                <InputText id="itRuc" />
+                                <label htmlhtmlFor="itRuc" style={{ fontSize: '0.8em' }}>Numero Ruc</label>
+                            </spa>
+                        </div>
+                    </div>
+
+                   <di className="row">
+                    
+                   </di>
+
+                </div>
+
+            </Dialog>
+        );
+    }
+
+
+}
