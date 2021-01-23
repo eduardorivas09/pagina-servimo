44ac25e8638e7f5f3c33bc473c53bf4bc2451b72
josuereyes <josuereyeschevy@gmail.com>
2021-01-22 21:03:33 -0600

    MUestar mensaje la momento de tratar iniciar sesion y no ingresar las creadenciales.

--- src/components/views/clientes/natural/ClienteNaturalModal.js
+++ src/components/views/clientes/natural/ClienteNaturalModal.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import {Dialog} from "primereact/dialog";
 import {InputText} from "primereact/inputtext";
 import {SelectButton} from "primereact/selectbutton";
@@ -11,7 +11,17 @@ export default class ClienteNaturalModal extends React.Component{
     constructor() {
         super();
         this.state = {
-            visible : true
+            visible : true,
+            ncedula : '',
+            pnombre : '',
+            snombre : '',
+            papelli : '',
+            sapelli : '',
+            genero : '',
+            estadoC : '',
+            telefono: '',
+            correo : '',
+            direcicon : ''
         }
 
         this.onHide = this.onHide.bind(this);
@@ -25,6 +35,7 @@ export default class ClienteNaturalModal extends React.Component{
     }
 
     onYesClick = () => {
+        console.log(this.state)
         alert('Sin funcionalidad. Estamos trabajando en las nuevas opciones!!')
     }
 
@@ -39,7 +50,6 @@ export default class ClienteNaturalModal extends React.Component{
     }
 
     render() {
-
         return (
             <Dialog  className="p-dialog" role="alert"
                      header={"Persona Natural"}
@@ -52,9 +62,9 @@ export default class ClienteNaturalModal extends React.Component{
 
                     <div className="container m-0">
                         <div className="row">
-                            <div className="col col-12 col-sm-12 col-md-12 col-lg-4">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-6">
                                 <span className="p-float-label" style={{marginTop:'1.3em'}}>
-                                    <InputText id="itCedula"  />
+                                    <InputText id="itCedula" onChange={(e) => this.setState({ncedula : e.target.value})} />
                                     <label htmlhtmlFor="itCedula" style={{fontSize: '0.8em'}}>Numero de Cedula</label>
                                 </span>
                             </div>
@@ -63,25 +73,25 @@ export default class ClienteNaturalModal extends React.Component{
                         <div className="row">
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-3" style={{marginTop:'1.3em'}}>
                                 <span className="p-float-label">
-                                    <InputText id="itPrimerNombre" />
+                                    <InputText id="itPrimerNombre" onChange={(e) => this.setState({pnombre : e.target.value})}  />
                                     <label htmlhtmlFor="itPrimerNombre" style={{fontSize: '0.8em'}}>Primer Nombre</label>
                                 </span>
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-3" style={{marginTop:'1.3em'}}>
                                 <span className="p-float-label">
-                                    <InputText id="itSegundoNombre" />
+                                    <InputText id="itSegundoNombre" onChange={(e) => this.setState({snombre : e.target.value})}  />
                                     <label htmlhtmlFor="in" style={{fontSize: '0.8em'}}>Segundo Nombre</label>
                                 </span>
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-3">
                                 <span className="p-float-label" style={{marginTop:'1.3em'}}>
-                                    <InputText id="itPrimerApellido"  />
+                                    <InputText id="itPrimerApellido" onChange={(e) => this.setState({papelli : e.target.value})}  />
                                     <label htmlhtmlFor="itPrimerApellido" style={{fontSize: '0.8em'}}>Primer Apellido</label>
                                 </span>
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-3 " style={{marginTop:'1.3em'}}>
                                 <span className="p-float-label">
-                                    <InputText id="itSegundoApellido" />
+                                    <InputText id="itSegundoApellido" onChange={(e) => this.setState({sapelli : e.target.value})} />
                                     <label htmlhtmlFor="itSegundoApellido" style={{fontSize: '0.8em'}}>Segundo Apellido</label>
                                 </span>
                             </div>
@@ -91,23 +101,23 @@ export default class ClienteNaturalModal extends React.Component{
                         </div>
                         <div className="row">
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-3" style={{marginTop:'1.3em'}}>
-                                <Dropdown options={[{name: 'Masculino'}, {name: 'Femenino'}]} optionLabel="name" placeholder="Genero" />
+                                <Dropdown onChange={(e) => this.setState({genero : e.target.value})} options={[{name: 'Masculino'}, {name: 'Femenino'}]} optionLabel="name" placeholder="Genero" />
                             </div>
 
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-3" style={{marginTop:'1.3em'}}>
-                                <Dropdown options={[{name: 'Soltero'}, {name: 'Casado'}]} optionLabel="name" placeholder="Estado Civil" />
+                                <Dropdown onChange={(e) => this.setState({estadoC : e.target.value})}  options={[{name: 'Soltero'}, {name: 'Casado'}]} optionLabel="name" placeholder="Estado Civil" />
                             </div>
 
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-3" style={{marginTop:'1.3em'}}>
                                 <span className="p-float-label">
-                                    <InputText id="itTelefono" />
+                                    <InputText id="itTelefono" onChange={(e) => this.setState({telefono : e.target.value})}/>
                                     <label htmlhtmlFor="itTelefono" style={{fontSize: '0.8em'}}>Telefono</label>
                                 </span>
                             </div>
 
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-3" style={{marginTop:'1.3em'}}>
                                 <span className="p-float-label">
-                                    <InputText id="itCorreo" />
+                                    <InputText id="itCorreo" onChange={(e) => this.setState({correo : e.target.value})}/>
                                     <label htmlhtmlFor="itCorreo" style={{fontSize: '0.8em'}}>Correo</label>
                                 </span>
                             </div>
@@ -116,7 +126,7 @@ export default class ClienteNaturalModal extends React.Component{
                         <div className="row">
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12" style={{marginTop:'1.3em'}}>
                                 <span className="p-float-label">
-                                    <InputTextarea id='itaDireccion' autoResize={true}/>
+                                    <InputTextarea id='itaDireccion' onChange={(e) => this.setState({direcicon : e.target.value})} autoResize={true}/>
                                     <label htmlhtmlFor="itaDireccion">Direccion</label>
                                 </span>
                             </div>
