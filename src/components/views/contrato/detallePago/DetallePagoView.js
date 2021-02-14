88dcc9b04f4f6f4902c73f9dd2a32a3636bb54c9
josuereyes <josuereyeschevy@gmail.com>
2021-02-14 16:57:25 -0600

    Comienza a realizar pantalla de detalle pago.

--- src/components/views/contrato/detallePago/DetallePagoView.js
+++ src/components/views/contrato/detallePago/DetallePagoView.js
@@ -1,5 +1,7 @@
 import React, {Fragment} from 'react';
 import {GenericView} from "../../GenericView";
+import {InputText} from "primereact/inputtext";
+import {InputNumber} from "primereact/inputnumber";
 
 export default class DetallePagoView extends GenericView{
     constructor() {
@@ -11,6 +13,15 @@ export default class DetallePagoView extends GenericView{
             <Fragment>
                 <div className="container">
                     <div className="row">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-6">
+                            <span className="p-float-label" style={{ marginTop: '1.3em' }}>
+                                <InputNumber id="itCedula"
+                                           value={this.state.noCedula}
+                                           onChange={(e) => this.setState({ ncedula: e.target.value })}
+                                           keyfilter={/[^\s]/} />
+                                <label htmlFor="itCedula" style={{ fontSize: '0.8em' }}>Numero de Cedula</label>
+                            </span>
+                        </div>
 
                     </div>
                 </div>
