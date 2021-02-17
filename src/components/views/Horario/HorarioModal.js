08136a80aac246042563265df5f70a912e4291a9
guismarck <guismarck01@gmail.com>
2021-02-17 00:57:05 -0600

    cambios

---
+++ src/components/views/Horario/HorarioModal.js
@@ -0,0 +1,19 @@
+import GenericModal from "../modal/GenericModal";
+import React from "react";
+
+
+export default class HorarioModal extends GenericModal {
+
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+
+    }
+
+
+}
