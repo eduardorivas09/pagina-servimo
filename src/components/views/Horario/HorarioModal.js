1fed39929c39b863d53ada95a7a569b23545eea5
guismarck <guismarck01@gmail.com>
2021-02-18 15:39:27 -0600

    cambios

--- src/components/views/Horario/HorarioModal.js
+++ src/components/views/Horario/HorarioModal.js
@@ -1,5 +1,7 @@
 import GenericModal from "../modal/GenericModal";
 import React from "react";
+import { Dialog } from "primereact/dialog";
+import { TabView } from "primereact/tabview";
 
 
 export default class HorarioModal extends GenericModal {
@@ -7,11 +9,53 @@ export default class HorarioModal extends GenericModal {
     constructor() {
         super();
         this.state = {
+            id: -1,
+            CodigoTrabajador: '',
+            HoraEntrada: '',
+            HoraSalida: '',
+            Turno: '',
+            TipoCliente: '',
+            Nombrecliente: '',
+            TrabajadorInactivo: [],
+            estado: false
 
         }
     }
 
-    render() {
+    renderFooter = () => {
+        return (
+            <div style={{ marginTop: '1em' }}>
+                <Button label="No" icon="pi pi-times" onClick={this.props.onClickNoButton} className="p-button-text" />
+                <Button label="Yes" icon="pi pi-check" onClick={this.props.onClickYesButton} autoFocus />
+            </div>
+        );
+    }
+
+    setHorario = (horario) => {
+
+    }
+
+    getHorario = () => {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+
+
+    toRender = () => {
+        return (
+
+            <div className="container m-0">
+              <TabView>
+                  
+              </TabView>
+            </div>
+
+        );
+
 
     }
 
